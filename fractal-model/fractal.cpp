fd67f9f057aeb32cab2bdd321b4834fa0711daf0
burenotti <burenotti@gmail.com>
2021-03-16 20:15:44 +0300

    Issue fixed: implementation saved partially

--- fractal-model/fractal.cpp
+++ fractal-model/fractal.cpp
@@ -10,3 +10,34 @@ Property* FractalModel::itemAt(int index)
 
     return nullptr;
 }
+
+bool FractalModel::hasIndex(int index)
+{
+  return index >= 0 && index <= size();
+}
+
+void FractalModel::insertAt(int index, Property* prop)
+{
+  m_propertyList.insert(index, prop);
+}
+
+void FractalModel::append(Property* prop)
+{
+  m_propertyList.append(prop);
+}
+
+int FractalModel::size()
+{
+  return m_propertyList.size();
+}
+
+void FractalModel::remove(int index)
+{
+  m_propertyList.removeAt(index);
+}
+
+void FractalModel::clear()
+{
+  m_propertyList.clear();
+}
+
